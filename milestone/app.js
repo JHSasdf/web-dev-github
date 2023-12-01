7e0f865355a291afaeff9254c3a19cfc0b928b1f
선지훈 <sgh4760@naver.com>
2023-12-01 22:12:50 +0900

    study:231201

--- milestone/app.js
+++ milestone/app.js
@@ -4,12 +4,10 @@ const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views/customer/auth'));
-
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/views'));
 
+app.use(express.static('public'));
 app.use(authRoutes);
 
 app.listen(3000);
