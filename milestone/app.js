367c9542521fec2a07855ff40dad6c182d29b7d4
선지훈 <sgh4760@naver.com>
2023-12-05 21:40:24 +0900

    study:231205

--- milestone/app.js
+++ milestone/app.js
@@ -15,6 +15,7 @@ const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const authRoutes = require('./routes/auth.routes');
 const productRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
+const adminRoutes = require('./routes/admin.routes');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/views'));
@@ -34,6 +35,7 @@ app.use(checkAuthStatusMiddleware);
 app.use(authRoutes);
 app.use(baseRoutes);
 app.use(productRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
 
--- milestone/util/authentication.js
+++ milestone/util/authentication.js
@@ -1,5 +1,6 @@
 function createUserSession(req, user, action) { // user는 mongodb에서 오는 user
     req.session.uid = user._id.toString();
+    req.session.isAdmin = user.isAdmin;
     req.session.save(action);
 }
 
